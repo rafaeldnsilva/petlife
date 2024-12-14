c051b8036f2c0df77f9d62611f28882b94daca92
Rafael do Nascimento Silva <rafaeldnsilva@gmail.com>
2024-12-14 09:54:25 -0300

    Primeiro commit da página PetLife

---
+++ index.js
@@ -0,0 +1,8 @@
+let elementosDuvida = document.querySelectorAll('.duvida');
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa');
+    })
+    
+});
